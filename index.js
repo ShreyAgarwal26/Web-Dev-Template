a2cf2147c6c81a2d3885b7af0298be12a861d0d8
IntellectualGuy <>
2021-04-27 19:52:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const https = require('https');
 const fs = require('fs');
 const request = require('request');
 const app = express();
+const port = process.env.port || 3000.
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('index', {}));
+app.get('/', (req, res) => res.render('index', {
+	foo:"FOO"
+}));
 
-app.listen(3000, () => console.log("Server started on port 3000."));	
+app.listen(port, () => console.log(`Server started on port ${port}.`));	
