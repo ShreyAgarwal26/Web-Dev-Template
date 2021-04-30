208e9dfcdee22a1b327ed22297639645c67fa7a7
IntellectualGuy <>
2021-04-30 14:57:28 +0000

    Add favicon and Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.get('/', (req, res) => res.render('index', {
 }));
 
 // To use when not using EJS
-// app.get('/',(req,res) => res.sendFile(`${__dirname}/index.html`));
+// app.get('/',(req,res) => res.sendFile(`${__dirname}/views/index.html`));
 
 app.listen(port, () => console.log(`Server started on port ${port}.`));	
