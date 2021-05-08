07642500ec9bcc58a656c5ac9f44e6cf50ae3c78
IntellectualGuy <>
2021-05-08 20:32:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -13,15 +13,20 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 // Include line 14 if only using ejs
 app.set('view engine', 'ejs');
-mongoose.connect('mongodb://localhost:27017/dbname', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// mongoose.connect('mongodb://localhost:27017/dbname', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
 app.get('/', (req, res) => res.render('index', {
 	foo:"FOO",
 	delimeter:"%"
 }));
+
+app.get('/:id',(req,res) => {
+	console.log("Parameter is " + req.params.id);
+	res.redirect('/');
+});
 
 app.get("*",(req,res) => res.render('404'))
 
@@ -31,13 +36,13 @@ app.get("*",(req,res) => res.render('404'))
 
 app.post('/', (req,res) => res.redirect('/'));
 
-const exampleSchema = new mongoose.Schema({
-  name: String
-});
-const Example = mongoose.model("example", exampleSchema);
-const example = new Example({
-  name: "Example"
-});
-example.save();
+// const exampleSchema = new mongoose.Schema({
+//   name: String
+// });
+// const Example = mongoose.model("example", exampleSchema);
+// const example = new Example({
+//   name: "Example"
+// });
+// example.save();
 
 app.listen(port, () => console.log(`Server started on port ${port}.`));	
