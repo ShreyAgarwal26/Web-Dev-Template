c22184b83f663e27421bb44003b23bb7eef1220d
IntellectualGuy <>
2021-05-14 16:33:18 +0000

    Add react

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,13 @@ import * as THREE from 'three';
 import _ from 'lodash';
 import hljs from 'highlight.js';
 import $ from "jquery";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<h1>Hello World!</h1>,
+	document.getElementById('root')
+);
 
 // Using CDN
 // import * as THREE from 'https://cdn.skypack.dev/three@r128';
